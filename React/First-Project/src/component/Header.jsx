b2cbd038b1917ebff31656df3e448af722a7caba
Naveen Kumawat <naveendimond77@gmail.com>
2025-08-31 19:43:57 +0530

    Initialize React project with Vite, add basic structure and components
    - Created package.json with dependencies and scripts for development
    - Added Vite configuration file for project setup
    - Implemented main application entry point (main.jsx)
    - Developed App component with a Header component
    - Created Header component with navigation items
    - Added placeholder components for About and Home
    - Included SVG assets for branding
    - Set up ESLint for code linting

---
+++ React/First-Project/src/component/Header.jsx
@@ -0,0 +1,14 @@
+function Header(){
+ return(
+  <>
+  <div className="">
+
+   <div className="">Home</div>
+   <div className="">Contact</div>
+   <div className="">Aboot</div>
+   <div className="">Services</div>
+  </div>
+  </>
+ )
+}
+export default Header
