78ac015093e7e519c98b25b6a9f0ffd1c91ec7b6
Naveen Kumawat <naveendimond77@gmail.com>
2025-09-09 11:20:33 +0530

    feat: Add HTML structure and JavaScript functionality for bulb toggle effect

---
+++ Javascript/Function.js
@@ -0,0 +1,15 @@
+function sum(...a){
+  for(let i=0 ; i<a.length ; i++){
+    console.log(a[i])
+  }
+
+}
+let arr =[10,20,1,2,34,5,5,6,6,6,66,6,7,7,4,4,6,77]
+
+sum(arr)
+
+const hello = () => {
+  console.log("Hello")
+}
+
+hello()
---
+++ Javascript/a.py
@@ -0,0 +1,10 @@
+a=10
+
+def sub():
+  print("sub")
+
+
+
+
+num= (input("enter the number"))
+print(type(num))
