bc8af622ad89b253ea176d82f188a6370056eae5
Naveen Kumawat <naveendimond77@gmail.com>
2025-10-13 22:11:07 +0530

    update

--- Project/SenddataTObackend/Frontend/src/App.jsx
+++ Project/SenddataTObackend/Frontend/src/App.jsx
@@ -22,7 +22,7 @@ function App() {
         <Navbar />
         <main>
           <Routes>
-            <Route path="/" element={<Home />} />
+            <Route path="/" element={<Home/>} />
             <Route path="/login" element={<Login />} />
             <Route path="/register" element={<Register />} />
 
